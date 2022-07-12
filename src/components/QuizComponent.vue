7751b903b83c959238c492e1fbbd61afca676ebf
Ethan Vachon <Ethan.Vachon@jacksons.com>
2022-07-12 09:12:09 -0600

    setup

---
+++ src/components/QuizComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-100 h-100 d-flex justify-content-around align-items-center" v-if="studyMethod == 'Tab to Note'">
+    <div class="card">test</div>
+    <div class="card">test</div>
+  </div>
+  <div class="w-100 h-100 d-flex justify-content-around align-items-center" v-if="studyMethod == 'Note to Tab'">
+    <div class="card">test2</div>
+    <div class="card">test2</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuizComponent',
+  props: {
+    studyMethod: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand fw-bold" href="#">Tab Teacher</a>
+      <select class="rounded p-1" v-model="studyMethod">
+        <option value="Note to Tab">Note to Tab</option>
+        <option value="Tab to Note">Tab to Note</option>
+      </select>
+  </nav>
+  <div class="height">
+    <QuizComponent :studyMethod="studyMethod"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import QuizComponent from '@/components/QuizComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    QuizComponent
+  },
+  data () {
+    return {
+      studyMethod: 'Note to Tab'
+    }
   }
 }
 </script>
+
+<style>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.height {
+  height: 50vh;
+}
+</style>
