f71849d74dc01b3c7c60d82d296359e3057d94ce
Ethan Vachon <Ethan.Vachon@jacksons.com>
2022-07-20 16:37:44 -0600

    done for now, having problems with minor modes

--- src/components/ProgressionPickerComponent.vue
+++ src/components/ProgressionPickerComponent.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="row my-2">
     <div class="col-12 d-flex justify-content-center">
-      <div class="card shadow py-2 px-4 mt-2">
-        <button class="btn btn-primary" @click="generateScale()">Start</button>
-        {{randomMode}}
-        {{randomScale}}
+      <div class="card shadow mt-2" v-if="randomProgression">
+        <div class="d-flex border-bottom p-2">
+          <h5 class="m-0">{{randomScale[0]}} {{randomMode}}</h5>
+        </div>
+        <div class="d-flex justify-content-around p-2">
+          <p class="m-0 px-2" :key="index" v-for="(note, index) in randomProgression">{{randomScale[note - 1]}}</p>
+        </div>
+        <div class="d-flex justify-content-center py-2 border-top">
+          <button class="btn-sm btn-primary" @click="generateScale()">Generate</button>
+        </div>
       </div>
     </div>
   </div>
@@ -15,23 +21,25 @@ export default {
   name: 'ProgressionPickerComponent',
   mounted () {
     this.generateScale()
+    this.generateProgression()
   },
   data () {
     return {
       notes: ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B', 'C'],
-      tonics: ['C', 'Db', 'D', 'Eb', 'E', 'F', 'G', 'Ab', 'A', 'Bb', 'B'],
+      //   tonics: ['C', 'Db', 'D', 'Eb', 'E', 'F', 'G', 'Ab', 'A', 'Bb', 'B'],
+      tonics: ['Db'],
+      progressions: [
+        [1, 5, 4, 6]
+      ],
 
       randomMode: null,
-      randomScale: null
+      randomScale: null,
+      randomProgression: null
     }
   },
   methods: {
     generateScale () {
-    //   const major = [0, 2, 4, 5, 7, 9, 11, 12]
-    //   const minor = [0, 2, 3, 5, 7, 8, 10, 12]
-    //   const harmonicMinor = [0, 2, 3, 5, 7, 8, 11, 12]
       const mode = this.generateMode()
-
       const tonic = this.tonics[Math.floor(Math.random() * this.tonics.length)]
       const scale = [tonic]
       for (let i = 1; i <= 7; i++) {
@@ -66,6 +74,9 @@ export default {
         this.randomMode = 'Harmonic Minor'
         return [0, 2, 3, 5, 7, 8, 11, 12]
       }
+    },
+    generateProgression () {
+      this.randomProgression = this.progressions[Math.floor(Math.random() * this.progressions.length)]
     }
   }
 }
